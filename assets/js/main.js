400dcbda0551fc912481f77cd4aa52e584e11a99
Stanislav Tsurkan <stas.tsurkan@gmail.com>
2022-10-27 00:55:27 +0300

    fff

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $(".header-navbar__btn").on("click", function () {
     $(this).toggleClass("header-navbar__btn--active");
-    $(".header-navbar__list").stop(true, true).fadeToggle(500);
+    $(".header-navbar__list").stop(true, true).slideToggle(500);
   });
 });
