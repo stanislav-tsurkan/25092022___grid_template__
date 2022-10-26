d6e1d9c9bfa823cac5dae7f1c4cc9fb5af6eb55a
Stanislav Tsurkan <stas.tsurkan@gmail.com>
2022-10-26 22:46:19 +0300

    26.10.2022

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
-
 $(document).ready(function () {
-
-  $('.header-navbar__hamburger').on('click', function () {
-    $(this).toggleClass('active');
-    $('.header-navbar__list').toggleClass('active');
+  $(".header-navbar__btn").on("click", function () {
+    $(this).toggleClass("header-navbar__btn--active");
+    $(".header-navbar__list")
+      .toggleClass("active")
+      .stop(true, true)
+      .slideToggle(500);
   });
-
 });
